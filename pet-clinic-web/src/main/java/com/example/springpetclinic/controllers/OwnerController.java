41f58fb83133710566d9d193be25de51f0c145ba
Rhys Jones <rhys.jones.92@hotmail.co.uk>
2020-11-06 19:47:04 +0000

    Add owner list index page and owner index controller. Closes #13.

---
+++ pet-clinic-web/src/main/java/com/example/springpetclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package com.example.springpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping("/owners")
+    public String listOwners(){
+
+        return "/owners/index";
+    }
+}
